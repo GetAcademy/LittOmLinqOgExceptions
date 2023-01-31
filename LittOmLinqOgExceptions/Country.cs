e21461c185d966f41fc96dee4c7a9aa897a94a07
GET Academy <terje@getacademy.no>
2023-01-31 09:58:40 +0100

    Initial commit

---
+++ LittOmLinqOgExceptions/Country.cs
@@ -0,0 +1,18 @@
+﻿namespace LittOmLinqOgExceptions
+{
+    internal class Country
+    {
+        public int Rank { get; }
+        public string Name { get; }
+        public int Population { get; }
+        public int Area { get; }
+
+        public Country(int rank, string name, int population, int area)
+        {
+            Area = area;
+            Population = population;
+            Name = name;
+            Rank = rank;
+        }
+    }
+}
---
+++ LittOmLinqOgExceptions/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ - litt om typiske oppgaver bedrift/codealong
+ - eksempel landene i verden inkl. rank, befolkning og areal
+ - med og uten linq
+ - LINQ
+   - to syntakser
+   - likhet med SQL
+   - extension method
+   - builder pattern
+   - anonyme metoder
+   - "cheat sheet": https://vslapp.files.wordpress.com/2011/11/linq-cheatsheet.pdf
+   
+ - ev. exceptions 
+ */
